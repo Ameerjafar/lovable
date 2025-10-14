1d9ff21ab4364193d56033b01db9d16de61c1034
Ameer jafar <ameerjafar123@gmail.com>
2025-10-14 16:20:00 +0530

    added llm

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/', routes);
+app.use("/", routes);
 
 app.listen(process.env.BACKEND_PORT, () => {
   console.log("port is running on", process.env.BACKEND_PORT);
--- apps/backend/llm/index.ts
+++ apps/backend/llm/index.ts
@@ -1,13 +1,17 @@
-import "dotenv/config";
-import { GoogleGenerativeAI } from "@google/generative-ai";
 
+import { GoogleGenerativeAI } from '@google/generative-ai';
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+console.log(genAI)
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-pro" });
 
-async function run() {
-  const model = genAI.getGenerativeModel({ model: "gemini-2.5-pro" });
-  console.log("above line");
-  const result = await model.generateContent("who is the president of america");
-  console.log(result.response.text());
+export async function streamTextResponse() {
+  const prompt = "president of america";
+  const result = await model.generateContentStream(prompt);
+
+  for await (const chunk of result.stream) {
+    const chunkText = chunk.text();
+    console.log(chunkText);
+  }
 }
 
-run();
+streamTextResponse();
